2f3986233b7e9f447abbef0cc7ba235efae56e7b
Seol Jae Yong <sjy2746@gmail.com>
2017-11-17 09:53:50 +0900

    - 1.13
    푸시 로그 추가

---
+++ jasoncommon/src/main/java/jason/common/util/PushLogCheckUtil.java
@@ -0,0 +1,52 @@
+package jason.common.util;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by pyo on 2017-11-15.
+ */
+
+public class PushLogCheckUtil {
+
+    // 푸시 로그 텍스트 파일로 downloads 에 저장
+    public static void logOutPut(Bundle data){
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+
+        String pushLog = sdf.format(date) + "\n" + data.toString().substring(6);
+
+        File file = new File(dirPath + "/androidPushLog_simsale.txt");
+        Log.e("file Path ::", file.getAbsolutePath());
+
+        try {
+            if (file.exists() == true) {
+                String encodeLog = new String(pushLog.getBytes("UTF-8"), "8859_1");
+                file.createNewFile();
+                RandomAccessFile raf = new RandomAccessFile(file.getAbsoluteFile(), "rw");
+                raf.seek(raf.length());
+                raf.writeBytes("\r\n" + encodeLog);
+                raf.close();
+
+            } else {
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(pushLog.getBytes("UTF-8"));
+                fileOutputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
